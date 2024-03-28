be8c66780405e520bcee138524b64c10e66b5331
imamura.kodai <imamura.kodai.1117@gmail.com>
2024-03-28 19:56:08 +0900

    最初のコミット

---
+++ WPFMVVMStandard/WPFMVVMStandard/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using WPFMVVMStandard.ViewModels;
+using WPFMVVMStandard.Views;
+
+namespace WPFMVVMStandard
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // MainView
+            var mainView = new MainView();
+            mainView.DataContext = new MainViewModel();
+
+            mainView.Show();
+        }
+    }
+
+}
---
+++ WPFMVVMStandard/WPFMVVMStandard/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WPFMVVMStandard.ViewModels
+{
+    public class MainViewModel
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
