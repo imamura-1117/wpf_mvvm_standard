c0689c5dc71e782f486cf1d63fd52a1011104429
imamura.kodai <imamura.kodai.1117@gmail.com>
2024-04-06 17:56:16 +0900

    ViewModelBaseを変更

--- WPFMVVMStandard/WPFMVVMStandard/ViewModels/ViewModelBase.cs
+++ WPFMVVMStandard/WPFMVVMStandard/ViewModels/ViewModelBase.cs
@@ -17,10 +17,35 @@ namespace WPFMVVMStandard.ViewModels
         /// プロパティの変更を通知します。
         /// </summary>
         /// <param name="propertyName">プロパティ名</param>
-        protected void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        protected void NotifyPropertyChanged([CallerMemberName]string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// プロパティを設定します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">設定されるフィールド</param>
+        /// <param name="value">設定する値</param>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns></returns>
+        protected bool SetProperty<T>(ref T field, T value,
+            [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+
+                return false;
+
+            }
+
+            field = value;
+
+            NotifyPropertyChanged(propertyName);
+
+            return true;
+
+        }
     }
 }
